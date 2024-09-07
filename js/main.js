2c57705219901e7b862d1dca7e5b9abeb78cd58f
SardorQobilov <sardorqobilov555@gmail.com>
2024-09-07 19:28:34 +0500

    project desctop 80% finished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+  AOS.init();
 
   // financial-status__slider
   const financialStatusSlider = new Swiper('.financial-status__slider', {
@@ -35,7 +36,7 @@ window.addEventListener('DOMContentLoaded', () => {
   let activeElement = null;
 
   userImages.forEach(img => {
-    img.addEventListener('click', function() {
+    img.addEventListener('click', function () {
       const userElement = this.closest('.business-networking__user');
       if (userElement) {
         if (activeElement && activeElement !== userElement) {
@@ -53,4 +54,45 @@ window.addEventListener('DOMContentLoaded', () => {
     spaceBetween: 12,
     speed: 700,
   });
+
+  const cards = document.querySelectorAll('.hero5-comparison__card-animation');
+
+  cards.forEach((card) => {
+    const image = card.querySelector('.animation-img');
+
+    card.addEventListener('mousemove', (e) => {
+      const cardRect = card.getBoundingClientRect();
+      const cardWidth = cardRect.width;
+      const cardHeight = cardRect.height;
+
+      const xPos = e.clientX - cardRect.left;
+      const yPos = e.clientY - cardRect.top;
+
+      const rotateX = ((yPos / cardHeight) - 0.5) * 30;
+      const rotateY = ((xPos / cardWidth) - 0.5) * -30;
+
+      image.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+
+    card.addEventListener('mouseleave', () => {
+      image.style.transform = `rotateX(0deg) rotateY(0deg)`;
+    });
+  });
+
+  try {
+    const cardsOne = document.querySelectorAll('.hero5-comparison__card');
+
+    cardsOne.forEach((card) => {
+      const plusBtn = card.querySelector('.plus-btn');
+      const cardInfo = card.querySelector('.hero5-comparison__card-info');
+
+      plusBtn.addEventListener('click', () => {
+        cardInfo.classList.toggle('active');
+        plusBtn.classList.toggle('active');
+      });
+    });
+
+  } catch (error) {
+
+  }
 });
