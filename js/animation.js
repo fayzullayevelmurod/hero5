c796577bdb48aadcb129d60c88bb692b01fceee3
SardorQobilov <note47297@gmail.com>
2024-09-11 09:49:49 +0500

    falling element fast

--- js/animation.js
+++ js/animation.js
@@ -26,42 +26,6 @@ function getRandomColor() {
   // return `hsl(${Math.floor(Math.random() * 360)}, 70%, 60%)`;
 }
 
-// Function to create and animate a falling element
-// function createFallingElement(element) {
-//   const div = document.createElement('div');
-//   const intro = document.querySelector('.intro-animation__elments');
-
-//   // Set styles based on element type (text or image)
-//   if (element.type === 'text') {
-//     div.className = 'falling-element';
-//     div.style.backgroundColor = getRandomColor(); // Assign a random background color
-//     div.textContent = element.content;
-//   } else if (element.type === 'image') {
-//     div.className = 'falling-image';
-//     const img = document.createElement('img');
-//     img.src = element.src;
-//     img.style.width = '100%';
-//     img.style.height = '100%';
-//     div.appendChild(img);
-//   }
-
-//   // Set random initial position and rotation
-//   const randomX = Math.random() * intro.offsetWidth;
-//   const randomRotation = Math.random() * 360 - 180;
-//   div.style.position = 'absolute'; // Make sure the position is absolute for proper placement
-//   div.style.left = `${randomX}px`;
-//   div.style.bottom = `107%`;
-//   div.style.transform = `rotate(${randomRotation}deg)`;
-
-//   // Append the element to the intro
-//   intro.appendChild(div);
-
-//   // Trigger the falling animation with a delay for each element
-//   setTimeout(() => {
-//     div.style.transform = `translateY(${intro.offsetHeight + 100}px) rotate(${randomRotation}deg)`;
-//     div.style.opacity = '1';
-//   }, 100);
-// }
 function createFallingElement(element) {
   const div = document.createElement('div');
   const intro = document.querySelector('.intro-animation__elments');
